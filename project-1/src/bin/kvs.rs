6aedbb12b40b5a9626efacaaddf3269ebdcd0a1f
MuZhou233 <muzhou233@outlook.com>
2021-03-22 22:36:30 +0800

    complete project-1

--- project-1/src/bin/kvs.rs
+++ project-1/src/bin/kvs.rs
@@ -1,3 +1,48 @@
+extern crate clap;
+use std;
+use clap::{Arg, App, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .subcommand(SubCommand::with_name("set")
+                    .arg(Arg::with_name("set")
+                        .long("set"))
+                    .arg(Arg::with_name("KEY")
+                        .required(true)
+                        .index(1))
+                    .arg(Arg::with_name("VALUE")
+                        .required(true)
+                        .index(2))
+                    )
+        .subcommand(SubCommand::with_name("get")
+                    .arg(Arg::with_name("get")
+                        .long("get"))
+                    .arg(Arg::with_name("KEY")
+                        .required(true)
+                        .index(1))
+                    )
+        .subcommand(SubCommand::with_name("rm")
+                    .arg(Arg::with_name("rm")
+                        .long("rm"))
+                    .arg(Arg::with_name("KEY")
+                        .required(true)
+                        .index(1))
+                    )
+        .get_matches();
+
+    if let Some(_matches) = matches.subcommand_matches("set") {
+        eprintln!("unimplemented");
+        std::process::exit(1);
+    }else if let Some(_matches) = matches.subcommand_matches("get") {
+        eprintln!("unimplemented");
+        std::process::exit(1);
+    }else if let Some(_matches) = matches.subcommand_matches("rm") {
+        eprintln!("unimplemented");
+        std::process::exit(1);
+    }else {
+        std::process::exit(1);
+    }
 }
--- project-1/src/lib.rs
+++ project-1/src/lib.rs
@@ -1,21 +1,42 @@
-pub struct KvStore {
+#![deny(missing_docs)]
+//! KvStore example
+//! ```rust
+//! use kvs::KvStore;
+//!
+//! let mut store = KvStore::new();
+//! store.set("key".to_owned(), "value".to_owned());
+//! assert_eq!(store.get("key".to_owned()), Some("value".to_owned()));
+//! ```
+use std::collections::HashMap;
 
+/// KvStore struct contains a std HashMap
+pub struct KvStore {
+    store: HashMap<String, String>,
 }
 
 impl KvStore {
+    /// return a new KvStore variable
     pub fn new() -> Self {
-        panic!()
+        KvStore {
+            store: HashMap::new(),
+        }
     }
 
-    pub fn set(&self, key: String, value: String) {
-        panic!()
+    /// this function package the HashMap::insert
+    pub fn set(&mut self, key: String, value: String) -> Option<String> {
+        self.store.insert(key, value)
     }
 
+    /// this function package the HashMap::get
     pub fn get(&self, key: String) -> Option<String> {
-        panic!()
+        match self.store.get(&key) {
+            Some(value) => Some(value.clone().to_owned()),
+            None => None,
+        }
     }
 
-    pub fn remove(&self, key: String) {
-        panic!()
+    /// this function package the HashMap::remove
+    pub fn remove(&mut self, key: String) -> Option<String> {
+        self.store.remove(&key)
     }
 }
