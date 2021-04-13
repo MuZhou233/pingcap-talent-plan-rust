4d23f3e9b99bb6bfbc80770058ed7197f90a4155
MuZhou233 <muzhou233@outlook.com>
2021-04-13 20:47:28 +0800

    truely complete project-1

--- project-1/src/bin/kvs.rs
+++ project-1/src/bin/kvs.rs
@@ -1,48 +1,40 @@
-extern crate clap;
 use std;
-use clap::{Arg, App, SubCommand};
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(name = "basic")]
+struct Opt {
+    #[structopt(subcommand)]
+    cmd: Option<OptKvs>
+}
+
+#[derive(StructOpt)]
+#[structopt(name = "main")]
+enum OptKvs {
+    Set {
+        #[structopt(name = "KEY")]
+        key: Option<String>,
+        #[structopt(name = "VALUE")]
+        value: Option<String>
+    },
+    Get {
+        #[structopt(name = "KEY")]
+        key: Option<String>
+    },
+    Rm {
+        #[structopt(name = "KEY")]
+        key: Option<String>
+    }
+}
 
 fn main() {
-    let matches = App::new(env!("CARGO_PKG_NAME"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .subcommand(SubCommand::with_name("set")
-                    .arg(Arg::with_name("set")
-                        .long("set"))
-                    .arg(Arg::with_name("KEY")
-                        .required(true)
-                        .index(1))
-                    .arg(Arg::with_name("VALUE")
-                        .required(true)
-                        .index(2))
-                    )
-        .subcommand(SubCommand::with_name("get")
-                    .arg(Arg::with_name("get")
-                        .long("get"))
-                    .arg(Arg::with_name("KEY")
-                        .required(true)
-                        .index(1))
-                    )
-        .subcommand(SubCommand::with_name("rm")
-                    .arg(Arg::with_name("rm")
-                        .long("rm"))
-                    .arg(Arg::with_name("KEY")
-                        .required(true)
-                        .index(1))
-                    )
-        .get_matches();
+    let opt = Opt::from_args();
 
-    if let Some(_matches) = matches.subcommand_matches("set") {
-        eprintln!("unimplemented");
-        std::process::exit(1);
-    }else if let Some(_matches) = matches.subcommand_matches("get") {
-        eprintln!("unimplemented");
-        std::process::exit(1);
-    }else if let Some(_matches) = matches.subcommand_matches("rm") {
-        eprintln!("unimplemented");
-        std::process::exit(1);
-    }else {
-        std::process::exit(1);
+    match opt.cmd {
+        Some(_) => {
+            eprintln!("unimplemented");
+        },
+        _ => {}
     }
+    std::process::exit(1);
 }
